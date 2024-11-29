18f106dab68f51e7933201eb3e12675e3e1f678f
Benjamin Chacko <benjaminchacko@outlook.com>
2024-11-29 02:42:17 +0000

    2nd commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let _hello = "hello";
     let _23 = 23;
-    let isLoggedIn = true;
-    println!("{}", isLoggedIn);
+    let is_logged_in = true;
+    println!("{}", is_logged_in);
 }
